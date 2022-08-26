c46700b78a5d6e58a5549d334c3f19d2e072ba01
zwb <1546841551@qq.com>
2022-08-26 10:04:19 +0800

    推送

--- service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductMainApplication.java
@@ -1,6 +1,7 @@
 package com.atguigu.gmall.product;
 
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
